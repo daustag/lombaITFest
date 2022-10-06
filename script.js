a95e6679224c425bc444369de2321604d1b3649a
Daus <110144266+daustag@users.noreply.github.com>
2022-10-06 18:33:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const burger = document.querySelector('.burger')
+const sidebar = document.querySelector('.sidebar')
+const bgSidebar = document.querySelector('.bg-sidebar')
+
+burger.addEventListener('click', function() {
+  this.classList.toggle('change')
+  sidebar.classList.toggle('change')
+  bgSidebar.classList.toggle('change')
+})
+
+bgSidebar.addEventListener('click', function() {
+  this.classList.remove('change')
+  sidebar.classList.remove('change')
+  burger.classList.remove('change')
+})
+$(document).ready(function(){
+  $(".dropdown-toggle").dropdown();
+});
+
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel();
+    
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+    
+  // Enable Carousel Controls
+  $(".carousel-control-prev").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".carousel-control-next").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
